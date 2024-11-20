26c80bb70096841dcfa4ebc60c1cca06423ce4e1
Phúc An Phan <phucanphan1995@gmail.com>
2024-11-20 08:45:17 +0700

    Create project

---
+++ src/main/java/com/mockproject/givetoget/GiveToGetApplication.java
@@ -0,0 +1,13 @@
+package com.mockproject.givetoget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveToGetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveToGetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mockproject/givetoget/GiveToGetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockproject.givetoget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveToGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
