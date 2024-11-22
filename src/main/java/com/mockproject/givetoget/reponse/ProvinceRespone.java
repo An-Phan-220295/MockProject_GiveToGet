88c9e2922df25fcaa24ed2569e349b528b2c34cd
BrianPham155 <2051120151@ut.edu.vn>
2024-11-22 14:41:07 +0700

    update code

---
+++ src/main/java/com/mockproject/givetoget/reponse/ProvinceRespone.java
@@ -0,0 +1,15 @@
+    package com.mockproject.givetoget.reponse;
+
+    import lombok.AllArgsConstructor;
+    import lombok.Builder;
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class ProvinceRespone {
+        private String code;
+        private String name;
+    }
